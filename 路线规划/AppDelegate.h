a7d79154343223160376b04db9e7f43e4f05ef32
jgrm <jgrm@jgrmdeMac-mini.local>
2017-04-05 11:24:02 +0800

    Initial Commit

---
+++ 路线规划/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  路线规划
+//
+//  Created by jgrm on 2017/4/5.
+//  Copyright © 2017年 klone1127. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
