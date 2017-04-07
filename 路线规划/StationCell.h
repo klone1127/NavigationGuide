7487cb2aa626353c61c053f80e3b171e6bc74779
klone1127 <840275654@qq.com>
2017-04-07 17:01:54 +0800

    经过站点 cell

---
+++ 路线规划/StationCell.h
@@ -0,0 +1,14 @@
+//
+//  StationCell.h
+//  路线规划
+//
+//  Created by jgrm on 2017/4/7.
+//  Copyright © 2017年 klone1127. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StationCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *stationLabel;
+
+@end
